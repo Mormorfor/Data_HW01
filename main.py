2f090d5210ff423f44832cf29ef4e2873eb1506c
Diana <diana.morgan@campus.technion.ac.il>
2022-04-17 14:13:12 +0300

    One big function

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ def main(argv):
  #   data.load_data(path, features)
     li =  [0]
     dt = data.load_data(path,features)
-    data.filter_by_feature(dt ,"is_holiday", li )
+  #  data.filter_by_feature(dt ,"is_holiday", li)
 
-    statistics.calc_stdv([1,2,4,5,6,10])
-    statistics.calc_covariance([1,2,4,5,6,10], [1,5,8,8,10,3])
-
+  #  statistics.calc_stdv([1,2,4,5,6,10])
+  #  statistics.calc_covariance([1,2,4,5,6,10], [1,5,8,8,10,3])
+    statistic_functions = [statistics.calc_mean,statistics.calc_stdv, statistics.calc_covariance]
+    winter_data = data.filter_by_feature(dt,"season",[3])
+    is_hol = [1]
+    winter_holiday_data = data.filter_by_feature(winter_data[0],"is_holiday",is_hol)
+    statistics.population_statistics("Winter", winter_holiday_data[0], "t1", "cnt", 13.0, True, statistic_functions)
 
 if __name__ == '__main__':
     main(sys.argv)
--- statistics.py
+++ statistics.py
@@ -1,8 +1,8 @@
 from math import sqrt
-
+import data
 
 def calc_mean(values):
-    return sum(values) / len(values)
+    return round(sum(values) / len(values), 2)
 
 def calc_stdv(values):
     mean = calc_mean(values)
@@ -12,7 +12,7 @@ def calc_stdv(values):
         sq_sum += (element ** 2)
     result = sqrt(sq_sum / (len(values)-1))
     print(result)
-    return result
+    return round(result, 2)
 
 def calc_covariance(values1, values2):
     mean_x = calc_mean(values1)
@@ -23,7 +23,23 @@ def calc_covariance(values1, values2):
         element += (val_x - mean_x)*(val_y - mean_y)
     result = element / (len(values1) - 1)
     print(result)
-    return result
+    return round(result, 2)
 
-def population_statistics(feature_description, data, treatment, target, threshold, is_above, statistic_functions):
-    pass
+def population_statistics(feature_description, input_data, treatment, target, threshold, is_above, statistic_functions):
+    print(feature_description)
+    values_above_threshold = []
+
+    for val in input_data[treatment]:
+        if val > threshold:
+            values_above_threshold.append(val)
+
+    filtered_data = data.filter_by_feature(input_data, treatment, values_above_threshold)
+    if(is_above):
+        print(f"{target}: {statistic_functions[0](filtered_data[0][target])}, {statistic_functions[1](filtered_data[0][target])}")
+    else:
+        print(
+            f"{target}: {statistic_functions[0](filtered_data[1][target])}, {statistic_functions[1](filtered_data[1][target])}")
+
+
+
+
